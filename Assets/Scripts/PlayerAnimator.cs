85c450d381f93272ab18b72dd450c283d4e8062b
Kate Haravaya <kateazhevskaya@gmail.com>
2025-12-19 12:49:20 +0100

    Changed the old input system to a new one

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -8,6 +8,7 @@ public class PlayerAnimator : MonoBehaviour {
     [SerializeField] private Player player;
     
     private Animator animator;
+    
     private void Awake() {
         animator =  GetComponent<Animator>();
         

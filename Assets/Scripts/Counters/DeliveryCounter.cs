fd67754509e65e126147b23a726e49b179d825b5
Kate Haravaya <kateazhevskaya@gmail.com>
2026-03-19 14:16:45 +0100

    Added Delivery Counter and recipe delivery logic

---
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class DeliveryCounter : BaseCounter
+{
+    public override void Interact(Player player) {
+        if (player.HasKitchenObject()) {
+            if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject)) {
+                // Only accepts Plates
+                player.GetKitchenObject().DestroySelf();
+            }
+        }
+    }
+}

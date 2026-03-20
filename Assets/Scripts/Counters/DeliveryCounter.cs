693865bf3fe1cadcd765139b1258acdb8673e345
Kate Haravaya <kateazhevskaya@gmail.com>
2026-03-20 11:58:48 +0100

    Added logic to spawn dynamic visuals for Delivery Manager recipes

--- Assets/Scripts/Counters/DeliveryCounter.cs
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -6,6 +6,8 @@ public class DeliveryCounter : BaseCounter
         if (player.HasKitchenObject()) {
             if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject)) {
                 // Only accepts Plates
+                
+                DeliveryManager.Instance.DeliveryRecipe(plateKitchenObject);
                 player.GetKitchenObject().DestroySelf();
             }
         }

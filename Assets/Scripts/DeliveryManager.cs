f9f3bf211de38a7b54e63204af82b8b84e793ac2
Kate Haravaya <kateazhevskaya@gmail.com>
2026-03-23 16:14:00 +0100

    Added dynamic UI visuals for Delivery Manager recipes

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -1,9 +1,14 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 
 public class DeliveryManager : MonoBehaviour {
 
+    
+    public event EventHandler OnRecipeSpawned;
+    public event EventHandler OnRecipeCompleted;
     public static DeliveryManager Instance { get; private set; }
     [SerializeField] private RecipeListSO recipeListSO;
     
@@ -26,8 +31,9 @@ public class DeliveryManager : MonoBehaviour {
 
             if (waitingRecipeSOList.Count < waitingRecipesMax) {
                 RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[Random.Range(0, recipeListSO.recipeSOList.Count)];
-                Debug.Log(waitingRecipeSO.recipeName);
                 waitingRecipeSOList.Add(waitingRecipeSO);
+                
+                OnRecipeSpawned?.Invoke(this, EventArgs.Empty);
             }
         }
     }
@@ -59,8 +65,10 @@ public class DeliveryManager : MonoBehaviour {
 
                 if (plateContentsMatchesRecipe) {
                     // Player delivered the correct recipe!
-                    Debug.Log("Player delivered the correct recipe!");
+                    
                     waitingRecipeSOList.RemoveAt(i);
+                    
+                    OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
                     return;
                 }
             }
@@ -68,8 +76,13 @@ public class DeliveryManager : MonoBehaviour {
         
         //No matches found!
         // Player did not deliver a correct recipe
-        Debug.Log("Player did not deliver a correct recipe!");
-
+        
     }
+    
+    public List<RecipeSO> GetWaitingRecipeSOList() {
+        return waitingRecipeSOList;
+    }
+    
+    
 
 }
